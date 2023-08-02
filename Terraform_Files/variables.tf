95be97279c566b196a70d8e41455654f5c1fc34c
Amr Ashraf <82893114+amrashraf-web@users.noreply.github.com>
2023-08-02 16:46:20 +0300

    Update variables.tf

--- Terraform_Files/variables.tf
+++ Terraform_Files/variables.tf
@@ -1,3 +1,11 @@
 variable "region" {
   description = "The AWS region where the resources will be created."
 }
+
+variable "aws_access_key" {
+  description = "The Aws Access Key ID."
+}
+
+variable "aws_secret_key" {
+  description = "The Aws Secret Key ID."
+}
