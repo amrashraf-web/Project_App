838e2930a275dc6bca07e0e7772752548bb39464
Amr Ashraf <82893114+amrashraf-web@users.noreply.github.com>
2023-08-07 19:45:39 +0300

    Update BucketList.sql

--- MySQL-and-Python/MySQL_Queries/BucketList.sql
+++ MySQL-and-Python/MySQL_Queries/BucketList.sql
@@ -1,12 +1,58 @@
-CREATE TABLE `tbl_user` (
+CREATE TABLE `BucketList`.`tbl_user` (
   `user_id` BIGINT NOT NULL AUTO_INCREMENT,
   `user_name` VARCHAR(200) NULL,
   `user_username` VARCHAR(200) NULL,
   `user_password` VARCHAR(200) NULL,
-  PRIMARY KEY (`user_id`)
-);
+  PRIMARY KEY (`user_id`));
 
-CREATE TABLE `tbl_wish` (
+USE BucketList;
+
+INSERT INTO tbl_user
+values
+(10,'ahmed','ahmed','ahmed');
+
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_createUser`(
+    IN p_name VARCHAR(200),
+    IN p_username VARCHAR(100),
+    IN p_password VARCHAR(200)
+)
+BEGIN
+    if ( select exists (select 1 from tbl_user where user_username = p_username) ) THEN
+     
+        select 'Username Exists !!';
+     
+    ELSE
+     
+        insert into tbl_user
+        (
+            user_name,
+            user_username,
+            user_password
+        )
+        values
+        (
+            p_name,
+            p_username,
+            p_password
+        );
+     
+    END IF;
+END$$
+DELIMITER ;
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_validateLogin`(
+IN p_username VARCHAR(20)
+)
+BEGIN
+    select * from tbl_user where user_username = p_username;
+END$$
+DELIMITER ;
+
+
+CREATE TABLE `BucketList`.`tbl_wish` (
   `wish_id` int(11) NOT NULL AUTO_INCREMENT,
   `wish_title` varchar(45) DEFAULT NULL,
   `wish_description` varchar(5000) DEFAULT NULL,
@@ -16,49 +62,41 @@ CREATE TABLE `tbl_wish` (
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
 
-INSERT INTO tbl_user
-VALUES
-(10, 'ahmed', 'ahmed', 'ahmed');
-
-CREATE PROCEDURE sp_createUser (
-    IN p_name VARCHAR(200),
-    IN p_username VARCHAR(100),
-    IN p_password VARCHAR(200)
-)
-BEGIN
-    IF (SELECT EXISTS (SELECT 1 FROM tbl_user WHERE user_username = p_username)) THEN
-        SELECT 'Username Exists !!';
-    ELSE
-        INSERT INTO tbl_user (user_name, user_username, user_password)
-        VALUES (p_name, p_username, p_password);
-    END IF;
-END;
-
-CREATE PROCEDURE sp_validateLogin (
-    IN p_username VARCHAR(200)
-)
-BEGIN
-    SELECT * FROM tbl_user WHERE user_username = p_username;
-END;
-
-DROP PROCEDURE IF EXISTS sp_addWish;
-
-CREATE PROCEDURE sp_addWish (
+USE `BucketList`;
+DROP procedure IF EXISTS `BucketList`.`sp_addWish`;
+DELIMITER $$
+USE `BucketList`$$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_addWish`(
     IN p_title varchar(45),
 	IN p_description varchar(1000),
 	IN p_user_id bigint
 )
 BEGIN
-    INSERT INTO tbl_wish (wish_title, wish_description, wish_user_id, wish_date)
-    VALUES (p_title, p_description, p_user_id, NOW());
-END;
+	insert into tbl_wish(
+		wish_title,
+		wish_description,
+		wish_user_id,
+		wish_date
+	)
+	values
+	(
+		p_title,
+		p_description,
+		p_user_id,
+		NOW()
+	);
+END$$
+DELIMITER ;
+;
 
-
-DROP PROCEDURE IF EXISTS sp_GetWishByUser;
-
-CREATE PROCEDURE sp_GetWishByUser (
-    IN p_user_id bigint
+USE `BucketList`;
+DROP procedure IF EXISTS `sp_GetWishByUser`;
+DELIMITER $$
+USE `BucketList`$$
+CREATE PROCEDURE `sp_GetWishByUser` (
+IN p_user_id bigint
 )
 BEGIN
-    SELECT * FROM tbl_wish WHERE wish_user_id = p_user_id;
-END;
+    select * from tbl_wish where wish_user_id = p_user_id;
+END$$
+DELIMITER ;
